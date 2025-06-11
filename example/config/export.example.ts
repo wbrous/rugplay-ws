d28c6777c1bc5cd25034f10658fb05222d3eec5a
Gir0fa <wils@gir0fa.com>
2025-06-11 17:57:13 -0500

    Moves config files to example folder
    Moves configuration files to the example folder and updates the default Discord webhook configuration to be disabled.
    
    This change makes it clearer that these files are examples and should be customized by the user. It also ensures that no real Discord webhook is used by default.

--- example/config/export.example.ts
+++ example/config/export.example.ts
@@ -8,8 +8,8 @@ const exportConfig: ExportConfig = {
     },
 
     discord: {
-        enabled: true,
-        webhookUrl: 'https://discord.com/api/webhooks/1381388535726342294/5eOEHemp-TAiULuX3Bhkhr_jOPL6apdQGse_kGoi6XTYTvMVBqOtERZW_BEamuFIXEi4',
+        enabled: false,
+        webhookUrl: '',
         messageFormat: 'embed'
     }
 };
