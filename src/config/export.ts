5ecc6a16143143f3204ae595da442cdc9a22ece4
Gir0fa <wils@gir0fa.com>
2025-06-11 18:02:17 -0500

    Create export.ts

---
+++ src/config/export.ts
@@ -0,0 +1,18 @@
+import { ExportConfig } from "../types/config";
+
+const exportConfig: ExportConfig = {
+    file: {
+        enabled: true,
+        outputPath: './exports/data.json',
+        format: 'json'
+    },
+
+    discord: {
+        enabled: false,
+        webhookUrl: '',
+        messageFormat: 'embed'
+    }
+};
+
+export { exportConfig };
+
