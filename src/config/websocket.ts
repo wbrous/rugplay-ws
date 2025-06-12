b598c44c6b045001522f1080f7b6cf5ee40595d5
Gir0fa <wils@gir0fa.com>
2025-06-12 09:18:22 -0500

    Delete src/config directory

--- src/config/websocket.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketConfig } from '../types/config';
-
-const webSocketConfig: WebSocketConfig = {
-    url: 'wss://ws.rugplay.com/',
-    reconnectAttempts: 5,
-    reconnectDelay: 1000
-};
-
-export { webSocketConfig };
